9cddcd6ee0de9faf43b691d6d54bf4a97cb5606a
Ruchita Godse <158310655+ruchitagodse@users.noreply.github.com>
2026-03-11 18:10:42 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+
 import "@/styles/main.scss";
 import Head from "next/head";
 
@@ -5,7 +6,15 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Font Awesome Icons */}
+        <title>Noormeher</title>
+
+        {/* Favicons */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Font Awesome */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
@@ -18,7 +27,6 @@ export default function App({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600;700;800&family=Inter:wght@400;500;600&display=swap"
           rel="stylesheet"
         />
-        
       </Head>
 
       <Component {...pageProps} />
